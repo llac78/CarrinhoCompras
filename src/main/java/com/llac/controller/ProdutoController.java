31b00a856105fb100fd6174f81d6e5baac7e0f4a
llac78 <llac78@gmail.com>
2020-08-27 15:48:27 -0300

    Primeiro commit: criação do projeto

---
+++ src/main/java/com/llac/controller/ProdutoController.java
@@ -0,0 +1,43 @@
+package com.llac.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.llac.entidades.Produto;
+import com.llac.service.ProdutoService;
+
+//@RestController
+//@RequestMapping(value = "/produtos")
+@Controller
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoService service;
+	
+	@GetMapping(path = "/")
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> inserir(@RequestBody Produto produto){
+		
+		service.inserir(produto);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(produto.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(produto);
+	}
+
+}
---
+++ src/main/java/com/llac/repositorio/ProdutoRepositorio.java
@@ -0,0 +1,9 @@
+package com.llac.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.llac.entidades.Produto;
+
+public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
+
+}
