95ac2296b437a79363383a49ffa08a8c447972bb
llac78 <llac78@gmail.com>
2020-08-29 12:37:33 -0300

    Criação da página de cadastro de Produto

--- src/main/java/com/llac/service/ProdutoService.java
+++ src/main/java/com/llac/service/ProdutoService.java
@@ -1,18 +1,14 @@
 package com.llac.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.llac.entidades.Produto;
-import com.llac.repositorio.ProdutoRepositorio;
 
 @Service
 public class ProdutoService {
 	
-	@Autowired
-	private ProdutoRepositorio repositorio;
-
-	public Produto inserir(Produto produto) {
-		return repositorio.save(produto);
-	}
+//	@Autowired
+//	private ProdutoRepositorio repositorio;
+//
+//	public Produto inserir(Produto produto) {
+//		return repositorio.save(produto);
+//	}
 }
