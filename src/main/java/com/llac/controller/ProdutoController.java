886b9ac4aadef299396385566071ef136f67366d
llac78 <llac78@gmail.com>
2020-09-01 00:05:07 -0300

    Implementação da listagem de Produtos

--- src/main/java/com/llac/controller/ProdutoController.java
+++ src/main/java/com/llac/controller/ProdutoController.java
@@ -1,5 +1,8 @@
 package com.llac.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,26 +21,32 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoService service;
 	
+	private List<Produto> produtos = new ArrayList<>();
+	
 	@GetMapping("/cadastroProduto")
 	public String cadastroProduto( Model model) {
 		
+		produtos = listarProdutos();
+		
 		model.addAttribute("produto", new Produto());
+		model.addAttribute("produtos", produtos);
 		
 		return "cadastroProduto";
 	}
 
 	@PostMapping("/inserirProduto")
 	public String inserirProduto(@Valid Produto produto, BindingResult result){
-		
 		if(result.hasFieldErrors()) {
 			return "cadastroProduto";
 		}
-		
 		if(produto != null) {
 			service.inserir(produto);
 		}
-		
 		return "index";
 	}
 
+	public List<Produto> listarProdutos(){
+		return service.listar();
+	}
+	
 }
